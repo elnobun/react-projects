2a5655aa402e3ac1c362222c969a030b56e95bb6
elnobun <elnobun@gmail.com>
2021-12-23 23:09:01 +0000

    shopping categories listings

--- client/src/data.js
+++ client/src/data.js
@@ -22,4 +22,22 @@ export const sliderItems = [
         desc: "Dont Compromise on Style! Get Flat 50% off for New Arrivals.",
         bg: "fbf0f4"
     }
-]
+];
+
+export const categories = [
+    {
+        id: 1,
+        img: "https://images.pexels.com/photos/5886041/pexels-photo-5886041.jpeg",
+        title: "SHIRT STYLE!"
+    },
+    {
+        id: 2,
+        img: "https://images.pexels.com/photos/2983464/pexels-photo-2983464.jpeg",
+        title: "LOUNGEWEAR STYLE!"
+    },
+    {
+        id: 3,
+        img: "https://images.pexels.com/photos/5480696/pexels-photo-5480696.jpeg",
+        title: "LIGHT JACKETS!"
+    },
+];
